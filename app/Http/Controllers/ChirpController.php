a934c913321b356881550cdb48c537bce385739c
kolambar <kolamba.r@gmail.com>
2025-02-03 14:33:59 +0300

    chiprs are saving in db and belong to users (many to one)

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chirp;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
 class ChirpController extends Controller
@@ -28,9 +30,15 @@ class ChirpController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+    
+        $request->user()->chirps()->create($validated);
+    
+        return redirect(route('chirps.index'));
     }
 
     /**
--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chirp extends Model
 {
-    //
+    protected $fillable = ['message'];
 }
